e3403a9a740f1cb4e5c4853273e04cb73b11a24a
lilyanB <lilyan.bastien@gmail.com>
2023-10-12 16:42:33 +0200

    add network sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_RPC_URL = process.env.ALCHEMY_RPC_URL;
+const ALCHEMY_RPC_URL_POLYGON = process.env.ALCHEMY_RPC_URL_POLYGON;
+const ALCHEMY_RPC_URL_SEPOLIA = process.env.ALCHEMY_RPC_URL_SEPOLIA;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -9,8 +11,12 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: ALCHEMY_RPC_URL,
+        url: ALCHEMY_RPC_URL_POLYGON,
       },
+    },
+    sepolia: {
+      url: ALCHEMY_RPC_URL_SEPOLIA,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
